0c215161563678f24732986c3d9bd3ac3aae5a1f
unldenis <mehillideny13@gmail.com>
2024-06-05 17:08:42 +0200

    now working

---
+++ javalinfly-example/src/main/java/com/github/unldenis/Main.java
@@ -0,0 +1,14 @@
+package com.github.unldenis;
+
+import com.github.unldenis.javalinfly.Controller;
+import com.github.unldenis.javalinfly.Get;
+import com.github.unldenis.javalinfly.Response;
+
+@Controller(path = "/hello")
+public class Main {
+
+    @Get(roles = {"user", "admin"})
+    public void main() {
+        System.out.println("Hello world!");
+    }
+}
