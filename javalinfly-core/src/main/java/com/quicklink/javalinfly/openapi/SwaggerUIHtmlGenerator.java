1ea930744415d9f249700df8b0fd8e5e6582df95
Denis Mehilli <denis.mehilli@qlsol.com>
2024-06-27 15:08:46 +0200

    0.1.8rc15 - including swagger files with cdn

--- javalinfly-core/src/main/java/com/quicklink/javalinfly/openapi/SwaggerUIHtmlGenerator.java
+++ javalinfly-core/src/main/java/com/quicklink/javalinfly/openapi/SwaggerUIHtmlGenerator.java
@@ -13,52 +13,46 @@ import java.nio.file.Files;
 public class SwaggerUIHtmlGenerator {
 
 
-
     public static String generateSwaggerUIHtml(String openApiJson) {
-        String swaggerUiCss = ResourceUtil.readResourceFile("swagger-ui.css");
-        String swaggerUiBundleJs =  ResourceUtil.readResourceFile(
-            "swagger-ui-bundle.js");
-        String swaggerUiStandalonePresetJs =  ResourceUtil.readResourceFile(
-            "swagger-ui-standalone-preset.js");
-
-        StringBuilder swaggerUiTemplate = new StringBuilder()
-                .append("<!DOCTYPE html>\n")
-                .append("<html lang=\"en\">\n")
-                .append("<head>\n")
-                .append("    <meta charset=\"UTF-8\">\n")
-                .append("    <title>Swagger UI</title>\n")
-                .append("    <style>\n")
-                .append("    * {\n")
-                .append("        margin: 0;\n")
-                .append("        padding: 0;\n")
-                .append("    }\n")
-                .append(swaggerUiCss)
-                .append("    </style>\n")
-                .append("</head>\n")
-                .append("<body>\n")
-                .append("    <div id=\"swagger-ui\"></div>\n")
-                .append("    <script>\n")
-                .append(swaggerUiBundleJs)
-                .append("    </script>\n")
-                .append("    <script>\n")
-                .append(swaggerUiStandalonePresetJs)
-                .append("    </script>\n")
-                .append("    <script>\n")
-                .append("    window.onload = function() {\n")
-                .append("        const ui = SwaggerUIBundle({\n")
-                .append("            spec: ").append(openApiJson).append(",\n")
-                .append("            dom_id: '#swagger-ui',\n")
-                .append("            presets: [\n")
-                .append("                SwaggerUIBundle.presets.apis,\n")
-                .append("                SwaggerUIStandalonePreset\n")
-                .append("            ],\n")
-                .append("            layout: \"StandaloneLayout\"\n")
-                .append("        });\n")
-                .append("    }\n")
-                .append("    </script>\n")
-                .append("</body>\n")
-                .append("</html>\n");
-
-        return swaggerUiTemplate.toString();
+        return """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Swagger UI</title>
+                <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+            </head>
+            <body>
+            	<style>
+            	* {
+            		margin: 0;
+            		padding: 0;
+            	}
+            	</style>
+                <div id="swagger-ui"></div>
+                        
+                <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+                <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-standalone-preset.js"></script>
+                <script>
+                    window.onload = function() {
+                        const json = %s;
+                        
+                        const ui = SwaggerUIBundle({
+                            spec: json,
+                            dom_id: '#swagger-ui',
+                            presets: [
+                                SwaggerUIBundle.presets.apis,
+                                SwaggerUIStandalonePreset
+                            ],
+                            layout: "StandaloneLayout"
+                        });
+                        
+                        window.ui = ui;
+                    };
+                </script>
+            </body>
+            </html>
+            """.formatted(openApiJson);
     }
 }
