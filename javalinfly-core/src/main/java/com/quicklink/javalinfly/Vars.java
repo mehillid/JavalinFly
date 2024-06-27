d4839cd1d99ac1af410eee69e98ebdeec4338f94
Denis Mehilli <denis.mehilli@qlsol.com>
2024-06-27 12:03:29 +0200

    0.1.8rc5 - get resource file as stream of openapi schemas

--- javalinfly-core/src/main/java/com/quicklink/javalinfly/Vars.java
+++ javalinfly-core/src/main/java/com/quicklink/javalinfly/Vars.java
@@ -1,44 +1,14 @@
 package com.quicklink.javalinfly;
 
 import com.quicklink.javalinfly.openapi.SwaggerUIHtmlGenerator;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UncheckedIOException;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Objects;
-
 public class Vars {
 
-  public static final String RESOURCE_FILE_SPEC = "openapi-spec.json";
-
-
-//  public static String openApiSpec() {
-//    try {
-//      var path = Paths.get(
-//          Objects.requireNonNull(Vars.class.getClassLoader().getResource(RESOURCE_FILE_SPEC)).toURI());
-//
-//      var content = Files.readString(path);
-//
-//      return content;
-//
-//    } catch (IOException e) {
-//      throw new UncheckedIOException(e);
-//    } catch (URISyntaxException e) {
-//      throw new RuntimeException(e);
-//    }
-//  }
+  public static final String RESOURCE_FILE_SPEC = "openapiSpec.json";
 
 
   public static String openApiSpec() {
-    return SwaggerUIHtmlGenerator.readResourceFile(RESOURCE_FILE_SPEC);
+    return SwaggerUIHtmlGenerator.readResourceFile("/" + RESOURCE_FILE_SPEC);
   }
-
 
   private static String SWAGGER_UI = null;
 
